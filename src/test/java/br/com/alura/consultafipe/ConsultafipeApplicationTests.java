ab281892073a2d0de623d05ada13d63c70c87437
Marcelo Paschoal <marcelo.hlp@outlook.com>
2024-01-11 09:40:49 -0300

    Preparando ambiente

---
+++ src/test/java/br/com/alura/consultafipe/ConsultafipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.consultafipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultafipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
